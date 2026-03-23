2bca8dc3c350a292a52f7d7c0a2e3eb9fffc0021
hyukjin17 <chung.hy@northeastern.edu>
2026-03-23 05:18:01 -0400

    extract global variables into a single config file

---
+++ include/config.hpp
@@ -0,0 +1,22 @@
+/**
+ * Hyuk Jin Chung
+ * 3/21/2026
+ *
+ * Config variables
+ * Main access point for global variables
+ */
+
+#pragma once
+
+namespace Config {
+    // Physical ArUco Board Settings
+    inline const float MARKER_LENGTH = 1.0f;
+    inline const float MARKER_SEPARATION = 0.2f; 
+    inline const int COLS = 9;
+    inline const int ROWS = 6;
+
+    // File paths
+    inline const std::string CAL_IMG_FOLDER = "data/calibration_frames";
+    inline const std::string VIS_IMG_FOLDER = "data/bounding_box_vis";
+    inline const std::string CALIBRATION_FILE = "calibration_params.yml";
+}
