5293ae5665b5cb5f432f338c72df8b23b4656f2c
hyukjin17 <chung.hy@northeastern.edu>
2026-03-23 00:01:03 -0400

    implement helper to generate ArUco boards of different size

---
+++ src/generate_board.cpp
@@ -0,0 +1,34 @@
+/**
+ * Hyuk Jin Chung
+ * 3/21/2026
+ *
+ * Generates a grid of ArUco markers to be used for camera calibration
+ * There are no available boards online with defined distances, so custom board was needed
+ */
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/objdetect/aruco_board.hpp>
+#include <iostream>
+
+// define the layout of the board
+#define COLS 9
+#define ROWS 6
+// define the size in units of the marker size
+// keep this same as the values in the detector object
+#define MARKER_LENGTH 1.0f
+#define MARKER_SEPARATION 0.2f
+
+int main()
+{
+    // use the same dictionary as the detector
+    cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
+
+    // generate the ArUco board and save it as a png
+    cv::aruco::GridBoard board(cv::Size(COLS, ROWS), MARKER_LENGTH, MARKER_SEPARATION, dictionary);
+    cv::Mat boardImage;
+    board.generateImage(cv::Size(2800, 2000), boardImage, 50, 1);
+    cv::imwrite("ArUco_Board.png", boardImage);
+
+    std::cout << "Successfully generated and saved ArUco_Board.png!" << std::endl;
+    return 0;
+}
