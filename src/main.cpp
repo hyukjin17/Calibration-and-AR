90cfcef390b0f32c771d5bd6679f94551bb9d0c3
hyukjin17 <chung.hy@northeastern.edu>
2026-03-23 14:41:11 -0400

    load calibration info and draw virtual objects onto scene

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,10 @@
 #include <filesystem>
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <opencv2/core/utils/logger.hpp>
 #include "TargetDetector.hpp"
 #include "Calibrator.hpp"
+#include "Renderer.hpp"
 #include "config.hpp"
 
 int main(int argc, char *argv[])
@@ -18,9 +20,14 @@ int main(int argc, char *argv[])
     cv::Mat src, dst;
     TargetDetector td;
     Calibrator cal;
+    Renderer r;
     int savedCount = 1;
 
+    // Mute all OpenCV Info and Warning messages
+    cv::utils::logging::setLogLevel(cv::utils::logging::LOG_LEVEL_ERROR);
+
     cal.loadExistingCalImages(Config::CAL_IMG_FOLDER, savedCount);
+    bool isCalibrated = cal.loadCalibration(Config::CALIBRATION_FILE);
 
     cv::VideoCapture capdev(0);
     if (!capdev.isOpened())
@@ -40,9 +47,31 @@ int main(int argc, char *argv[])
 
         src.copyTo(dst);
         bool targetDetected = td.processFrame(dst);
-        cv::imshow("ArUco Detection Test", dst);
-        // std::cout << "Number of markers: " << td.getCornerSet().size() << std::endl;
 
+        if (targetDetected && isCalibrated)
+        {
+            std::vector<cv::Vec3f> pointSet = td.computePointSet();
+            std::vector<cv::Point2f> cornerSet = td.getFlattenedCorners();
+
+            // Solve for the current camera position
+            cv::Mat rvec, tvec;
+            bool success = cv::solvePnP(pointSet, cornerSet, cal.getCameraMatrix(), cal.getDistCoef(), rvec, tvec);
+
+            // draw a wireframe crystal near the origin (as a floating virtual object)
+            if (success)
+                r.drawCrystal(dst, rvec, tvec, cal.getCameraMatrix(), cal.getDistCoef());
+
+            // Print the real-time movement data
+            // std::cout << "Translation (tvec): [" << tvec.at<double>(0) << ", "
+            //           << tvec.at<double>(1) << ", " << tvec.at<double>(2) << "]" << std::endl;
+            // std::cout << "Rotation (rvec): [" << rvec.at<double>(0) << ", "
+            //           << rvec.at<double>(1) << ", " << rvec.at<double>(2) << "]" << std::endl;
+
+            // Draw the 3D axes attached to the origin of the board
+            cv::drawFrameAxes(dst, cal.getCameraMatrix(), cal.getDistCoef(), rvec, tvec, 2.0f);
+        }
+
+        cv::imshow("ArUco Detection Test", dst);
         char key = cv::waitKey(1);
         if (key == 'q')
             break;
@@ -70,20 +99,20 @@ int main(int argc, char *argv[])
             cal.calibrate(dst.cols, dst.rows);
             cal.saveCalibration(Config::CALIBRATION_FILE, Config::CAMERA_POSITION_FILE);
         }
-        else if (key == 'd') 
+        else if (key == 'd')
         {
             if (std::filesystem::exists(Config::CAL_IMG_FOLDER))
             {
                 // delete all image files in the folder
-                for (const auto& entry : std::filesystem::directory_iterator(Config::CAL_IMG_FOLDER))
+                for (const auto &entry : std::filesystem::directory_iterator(Config::CAL_IMG_FOLDER))
                 {
                     std::filesystem::remove(entry.path());
                 }
             }
-            
-            cal.clear(); 
+
+            cal.clear();
             savedCount = 1; // Reset the naming counter
-            
+
             std::cout << "All calibration images deleted" << std::endl;
         }
     }
