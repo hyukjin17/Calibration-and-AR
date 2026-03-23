6c24816f3f3303b91bbfe41a248ad81f1e829de1
hyukjin17 <chung.hy@northeastern.edu>
2026-03-23 05:25:16 -0400

    use config file variables instead of using TargetDetector object to extract that information

--- src/generate_board.cpp
+++ src/generate_board.cpp
@@ -9,17 +9,15 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/objdetect/aruco_board.hpp>
 #include <iostream>
-#include "TargetDetector.hpp"
+#include "config.hpp"
 
 int main()
 {
-    TargetDetector td; // used to extract grid size and marker size information
-
     // use the same dictionary as the detector
     cv::aruco::Dictionary dictionary = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_6X6_250);
 
     // generate the ArUco board and save it as a png
-    cv::aruco::GridBoard board(td.getGridSize(), td.getMarkerLength(), td.getMarkerSep(), dictionary);
+    cv::aruco::GridBoard board(cv::Size(Config::COLS, Config::ROWS), Config::MARKER_LENGTH, Config::MARKER_SEPARATION, dictionary);
     cv::Mat boardImage;
     board.generateImage(cv::Size(2800, 2000), boardImage, 50, 1);
     cv::imwrite("ArUco_Board.png", boardImage);
