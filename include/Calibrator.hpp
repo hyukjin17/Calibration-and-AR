21414fc95956cdbd14a5c4db859a6d8e0ae69072
hyukjin17 <chung.hy@northeastern.edu>
2026-03-23 02:26:02 -0400

    implement camera calibration in addition to getters and setters

--- include/Calibrator.hpp
+++ include/Calibrator.hpp
@@ -17,9 +17,28 @@ public:
     // Getter functions
     std::vector<std::vector<cv::Vec3f>> getPointList() const;
     std::vector<std::vector<cv::Point2f>> getCornerList() const;
-    void addCornersPoints(const std::vector<cv::Point2f>& cornerSet, const std::vector<cv::Vec3f>& pointSet);
+    int getNumCalFrames() const;
+
+    // Populates the list with all points identified in calibration images
+    void addCornersPoints(const std::vector<cv::Point2f> &cornerSet, const std::vector<cv::Vec3f> &pointSet);
+    // Calibrate the camera matrix
+    void calibrate(int cols, int rows);
 
 private:
 	std::vector<std::vector<cv::Vec3f>> pointList;
 	std::vector<std::vector<cv::Point2f>> cornerList;
+    int numCalFrames = 0;
+
+    // Calibration parameters
+    cv::Size imgSize;
+    cv::Mat distCoef;
+    cv::Mat cameraMatrix;
+    std::vector<cv::Mat> rvecs;
+    std::vector<cv::Mat> tvecs;
+    // flags assume square pixels, use the initial camera matrix, and keep all 8 values in the distortion coefficient vector
+    int flags = cv::CALIB_FIX_ASPECT_RATIO | cv::CALIB_USE_INTRINSIC_GUESS | cv::CALIB_RATIONAL_MODEL;
+
+    // Setter functions
+    void setImgSize(int cols, int rows);
+    void setCameraMatrix(int cols, int rows);
 };
--- src/Calibrator.cpp
+++ src/Calibrator.cpp
@@ -5,24 +5,75 @@
  * Using the 3D points defined by the TargetDetector, calibrates the camera
  */
 
+#include <iostream>
 #include "Calibrator.hpp"
 
 // Constructor
-Calibrator::Calibrator() {}
+Calibrator::Calibrator()
+{
+    distCoef = cv::Mat::zeros(8, 1, CV_64FC1);
+}
 
+// Getter functions
 std::vector<std::vector<cv::Vec3f>> Calibrator::getPointList() const
 {
     return pointList;
 }
-
 
 std::vector<std::vector<cv::Point2f>> Calibrator::getCornerList() const
 {
     return cornerList;
 }
 
-void Calibrator::addCornersPoints(const std::vector<cv::Point2f>& cornerSet, const std::vector<cv::Vec3f>& pointSet)
+int Calibrator::getNumCalFrames() const
+{
+    return numCalFrames;
+}
+
+// Sets the image size using the given dimensions
+void Calibrator::setImgSize(int cols, int rows)
+{
+    imgSize = cv::Size(cols, rows);
+}
+
+/**
+ * Sets the initial camera matrix as shown below
+ * 
+ * [1, 0, frame.cols/2]
+ * [0, 1, frame.rows/2]
+ * [0, 0, 1           ]
+ */
+void Calibrator::setCameraMatrix(int cols, int rows)
+{
+    cameraMatrix = cv::Mat::eye(3, 3, CV_64FC1);
+    cameraMatrix.at<double>(0, 2) = cols / 2.0;
+    cameraMatrix.at<double>(1, 2) = rows / 2.0;
+}
+
+// Adds extracted corner points from the calibration image into the combined list
+void Calibrator::addCornersPoints(const std::vector<cv::Point2f> &cornerSet, const std::vector<cv::Vec3f> &pointSet)
 {
     cornerList.push_back(cornerSet);
     pointList.push_back(pointSet);
+    numCalFrames++;
+}
+
+void Calibrator::calibrate(int cols, int rows)
+{
+    if (cornerList.size() < 5)
+    {
+        std::cout << "Please take at least 5 calibration images" << std::endl;
+        return;
+    }
+    
+    setImgSize(cols, rows);
+    setCameraMatrix(cols, rows);
+
+    std::cout << "Camera Matrix: " << std::endl << cameraMatrix << std::endl;
+    std::cout << "Distortion Coefficients: " << std::endl << distCoef << std::endl;
+    double reprojError = cv::calibrateCamera(pointList, cornerList, imgSize, cameraMatrix, distCoef, rvecs, tvecs, flags);
+    std::cout << "Camera Matrix: " << std::endl << cameraMatrix << std::endl;
+    std::cout << "Distortion Coefficients: " << std::endl << distCoef << std::endl;
+    std::cout << "Reprojection Error: " << reprojError << std::endl;
+
 }
